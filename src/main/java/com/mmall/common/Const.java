ae78b85417ee305d1293a6d7a1eaa087f926d66b
cherrygu1209 <cherrygu1209@gmail.com>
2018-09-04 22:10:52 +0800

    user register/logout/checkvalid function

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -3,4 +3,12 @@ package com.mmall.common;
 
 public class Const {
     public static final String CURRENT_USER = "currentUser";
+
+    public static final String EMAIL = "email";
+    public static final String USERNAME = "username";
+
+    public interface Role{
+        int ROLE_CUSTOMER = 0; //common customer
+        int ROLE_ADMIN = 1; //admin
+    }
 }
--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.mmall.service.impl;
 
+import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
 import com.mmall.dao.UserMapper;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import com.mmall.util.MD5Util;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,16 +22,60 @@ public class UserServiceImpl implements IUserService {
     public ServerResponse<User> login(String username, String password) {
         int resultCount = userMapper.checkUserName(username);
         if(resultCount == 0){
-            return ServerResponse.createByErrorMessage("User does not exist.");
+            return ServerResponse.createByErrorMessage("User does not exist");
         }
 
-        //todo 密码 MD5加密
+        //MD5加密
+        String md5Password = MD5Util.MD5EncodeUtf8(password);
 
-        User user = userMapper.selectLogin(username,password);
+        User user = userMapper.selectLogin(username,md5Password);
         if(user == null){
             return ServerResponse.createByErrorMessage("Wrong password");
         }
         user.setPassword(StringUtils.EMPTY);
         return ServerResponse.createBySuccessMessage("Login Success");
+    }
+
+    public ServerResponse<String> register(User user){
+        ServerResponse validResponse = this.checkValid(user.getUsername(),Const.USERNAME);
+        if(validResponse.isSuccess()){
+            return validResponse;
+        }
+
+        validResponse = this.checkValid(user.getEmail(),Const.EMAIL);
+        if(validResponse.isSuccess()){
+            return validResponse;
+        }
+
+        user.setRole(Const.Role.ROLE_CUSTOMER);
+        //MD5 加密
+        user.setPassword(MD5Util.MD5EncodeUtf8(user.getPassword()));
+
+        int resultCount = userMapper.insert(user);
+        if(resultCount == 0){
+            return ServerResponse.createByErrorMessage("Register Failed");
+        }
+        return ServerResponse.createBySuccessMessage("Register success");
+    }
+
+    public ServerResponse<String> checkValid(String str, String type){
+        if(org.apache.commons.lang3.StringUtils.isNoneBlank(type)){
+            //开始校验
+            if(Const.USERNAME.equals(type)){
+                int resultCount = userMapper.checkUserName(str);
+                if(resultCount > 0){
+                    return ServerResponse.createByErrorMessage("User has already existed");
+                }
+            }
+            if(Const.EMAIL.equals(type)){
+                int resultCount = userMapper.checkEmail(str);
+                if(resultCount > 0){
+                    return ServerResponse.createByErrorMessage("Email address has already existed");
+                }
+            }
+        }else{
+            return ServerResponse.createByErrorMessage("Argument Error");
+        }
+        return ServerResponse.createBySuccessMessage("Valid success");
     }
 }
